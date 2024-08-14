0a1e906007e9be76a8716a31634b84341615907f
lenny098 <32307017+lenny098@users.noreply.github.com>
2024-08-14 18:52:51 +0800

    Refactoring: Difficulty Button
    Changes:
    - Removed unused "using"
    - Added "RequireComponent()" for better reusability
    - Replaced unnecessary "public" and "private" with "[SerializeField]" or the default modifier
    - Removed empty function
    - Renamed function for better readability

--- Assets/Scripts/DifficultyButton.cs
+++ Assets/Scripts/DifficultyButton.cs
@@ -1,32 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Button))]
 public class DifficultyButton : MonoBehaviour
 {
-    private Button button;
-    private GameManager gameManager;
+    [SerializeField] Difficulty difficulty;
 
-    public Difficulty difficulty;
+    GameManager gameManager;
+
+    void StartGame()
+    {
+        gameManager.StartGame(difficulty);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        button = GetComponent<Button>();
-        button.onClick.AddListener(SetDifficulty);
-
         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void SetDifficulty()
-    {
-        gameManager.StartGame(difficulty);
+        GetComponent<Button>().onClick.AddListener(StartGame);
     }
 }
